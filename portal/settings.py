4f26b399af7e20f4b1976ef68986721d6dd5f5ab
samarth <samarthkathal@github.com>
2020-07-20 04:57:06 +0530

    changed db

--- portal/settings.py
+++ portal/settings.py
@@ -103,18 +103,18 @@ WSGI_APPLICATION = 'portal.wsgi.application'
 
 
 
-#DATABASES = {}
-#DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
 
 
 
